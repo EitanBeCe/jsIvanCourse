56b992e1f2fa1a5f77e9f2c9736242c2ba323aed
Eitan <eitanleviberger@gmail.com>
2022-05-07 21:30:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//alert ('Hello');
+
+/* const result = confirm ('are U here?');
+console.log(result); */
+
+// const answer = prompt('Есть 18?', 'Да/нет');
+// console.log(answer);
+
+const answers = [];
+answers[0] = prompt('имя', '');
+answers[1] = prompt('family', '');
+answers[2] = prompt('age', '');
+console.log(answers);
